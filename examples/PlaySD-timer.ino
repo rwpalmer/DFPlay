46f9d88927b35867f95bee060185b6b7b6188fdd
FedericoBusero <35894905+FedericoBusero@users.noreply.github.com>
2018-08-31 22:06:45 +0200

    begin(serial1)

--- examples/PlaySD-timer.ino
+++ examples/PlaySD-timer.ino
@@ -23,7 +23,8 @@
 DFPlay dfPlay;
 Timer timer(20,timerFunction);      // Runs dfPlay.manageDevice() every 20ms
 void setup() {
-    dfPlay.begin();					// Prepares DFPlay for execution
+    Serial1.begin(9600);
+    dfPlay.begin(Serial1);					// Prepares DFPlay for execution
     timer.start();
     dfPlay.setVolume(10);			// Sets volume level to 10 (valid range = 0 to 30)
     Selection SDcard = {2,0,0,0,0}; // Selects all tracks on the SD card
